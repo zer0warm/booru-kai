364e7c9b927e0e331cefc4789193866785bb0000
zer0warm <dqhieu.2016@gmail.com>
2020-10-13 03:45:14 +0700

    Update script
    This does two things:
    * Fix Get-ChildItem not searching for recursive directories
    * Add code for popping up completion message

--- blur-downloaded-image/get_local_md5_danbooru.ps1
+++ blur-downloaded-image/get_local_md5_danbooru.ps1
@@ -1,4 +1,9 @@
-$md5s = Get-ChildItem -Name -Filter "*drawn*by*" |
+$md5s = Get-ChildItem -Recurse -Name -Filter "*drawn*by*" |
 		Select-String -All -Pattern "[0-9a-f]{32}" |
 		ForEach { $_.Matches.Value }
+
 Set-Clipboard -Value "$md5s"
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "md5s saved to clipboard!"
+[System.Windows.MessageBox]::Show($msgBody)
